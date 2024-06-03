4984f9816e378f4196185929b0ea04156ef5e291
Cecilia Palavecino <cecilhia22@gmail.com>
2024-06-03 19:56:13 -0300

    Primer commit

---
+++ java.js
@@ -0,0 +1,20 @@
+let x=35;
+let y=50;
+
+let suma = x+y;
+console.log(suma);
+
+let resta= x-y;
+console.log(resta);
+
+let multiplica=x*y;
+console.log(multiplica);
+
+let divide=x/y;
+console.log(divide);
+
+let mayor = (x>=y);
+console.log(mayor);
+
+//let distinta = (x!=y);
+console.log(x!=y);
